0cb43bcab2afa1af6fed0ae9f7574e97c5ab29fd
D3strophe <58573091+D3strophe@users.noreply.github.com>
2022-03-27 22:04:37 -0400

    Create DESCRIPTION.R

---
+++ Package/DESCRIPTION.R
@@ -0,0 +1,8 @@
+Package: TwitterTokens
+Title: "Tweeter Tokenizer"
+Version: 0.1
+Authors@R: "Kapil Patel <patelk10@usf.edu> [Patel, 2022]"
+Description: "This package provides a clean interface to tokenize tweets via their development api."
+Depends: R (>= 3.1.2)
+License: GNU
+LazyData: true
